4c3fdf5c52c194f3c811cd9d189bc37a21e817a6
Mr.Salih <salihbaz07@gmail.com>
2023-05-05 14:39:14 +0300

    new printf

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -1,50 +1,51 @@
 #include <unistd.h>
 #include <stdarg.h>
-
-void	put_string(char *string, int *length)
+void put_str(char *str, int *len)
 {
-	if (!string)
-		string = "(null)";
-	while (*string)
-		*length += write(1, string++, 1);
+    int i = 0;
+    if (!str)
+        str = "(null)";
+    while (str[i])
+        *len += write(1, &str[i++], 1);
 }
-
-void	put_digit(long long int number, int base, int *length)
+void put_num(long long num, int base, int *len)
 {
-	char	*hexadecimal = "0123456789abcdef";
-
-	if (number < 0)
-	{
-		number *= -1;
-		*length += write(1, "-", 1);
-	}
-	if (number >= base)
-		put_digit((number / base), base, length);
-	*length += write(1, &hexadecimal[number % base], 1);
+    char *hexa = "0123456789abcdef";
+    if (num < 0)
+    {
+        num *= -1;
+        *len += write(1, "-", 1);
+    }
+    if (num >= base)
+        put_num(num/base, base, len);
+    *len += write(1, &hexa[num % base], 1);
 }
-
-int	ft_printf(const char *format, ...)
+int ft_printf(char *str, ...)
 {
-	int length = 0;
-
-	va_list	pointer;
-	va_start(pointer, format);
-
-	while (*format)
-	{
-		if ((*format == '%') && ((*(format + 1) == 's') || (*(format + 1) == 'd') || (*(format + 1) == 'x')))
-		{
-			format++;
-			if (*format == 's')
-				put_string(va_arg(pointer, char *), &length);
-			else if (*format == 'd')
-				put_digit((long long int)va_arg(pointer, int), 10, &length);
-			else if (*format == 'x')
-				put_digit((long long int)va_arg(pointer, unsigned int), 16, &length);
-		}
-		else
-			length += write(1, format, 1);
-		format++;
-	}
-	return (va_end(pointer), length);
+    int i = 0;
+    int len = 0;
+    va_list ptr;
+    va_start(ptr, str);
+    while (str[i])
+    {
+        if(str[i] == '%' && str[i + 1])
+        {
+            i++;
+            if (str[i] == 's')
+                put_str(va_arg(ptr, char *), &len);
+            else if (str[i] == 'd')
+                put_num((long long)va_arg(ptr, int), 10, &len);
+            else if (str[i] == 'x')
+                put_num((long long)va_arg(ptr, int), 16, &len);
+        }
+        else
+            len += write(1, &str[i], 1);
+        i++;
+    }
+    va_end (ptr);
+    return (len);
+}
+int main()
+{
+    ft_printf("Hexadecimal for %d is %x\n", 42, 42);
 }
